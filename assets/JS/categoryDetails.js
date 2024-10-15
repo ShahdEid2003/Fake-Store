7a3c4de37e391e79a63b522b05435def86bbd159
ShahdEid2003 <shahdeid012@gmail.com>
2024-10-15 15:54:58 +0300

    add loader and navScroll

--- assets/JS/categoryDetails.js
+++ assets/JS/categoryDetails.js
@@ -1,22 +1,31 @@
-const getProducts= async () => {
-    const params= new URLSearchParams(window.location.search);
-    const category =params.get('category');
-    const{data}= await axios.get(`https://dummyjson.com/products/category/${category}`);
+const getProducts = async () => {
+    const params = new URLSearchParams(window.location.search);
+    const category = params.get('category');
+    const { data } = await axios.get(`https://dummyjson.com/products/category/${category}`);
     return data;
-    
-}; 
+
+};
 const displayProducts = async () => {
-    const data= await getProducts();
-    const result = data.products.map((product) => {
-        return `
+    const loader = document.querySelector('.loader-container')
+    loader.classList.add('active');
+    try {
+        const data = await getProducts();
+        const result = data.products.map((product) => {
+            return `
         <div class="product">
             <img src='${product.thumbnail}' alt="${product.description}">
             <h3>${product.title}</h3>
            <span class="price">${product.price}</span>
         </div>
         `;
-    }).join(' ');
-    document.querySelector(".products .row").innerHTML = result;
-   
+        }).join(' ');
+        document.querySelector(".products .row").innerHTML = result;
+    }
+    catch (error) {
+        document.querySelector(".categories .row").innerHTML = `<p>Loading...</p>`;
+    }
+    {
+        loader.classList.remove('active');
+    }
 };
 displayProducts();
--- assets/JS/main.js
+++ assets/JS/main.js
@@ -5,16 +5,40 @@ const getCategory = async () => {
 
 };
 const displayCategory = async () => {
-    const Categories = await getCategory();
-    const result = Categories.map((category) => {
-        return `
+    const loader = document.querySelector('.loader-container')
+    loader.classList.add('active');
+    try {
+        const Categories = await getCategory();
+        const result = Categories.map((category) => {
+            return `
         <div class="category">
             <h2>${category}</h2>
             <a href="categoryDetails.html?category=${category}">Details</a>
         </div>
         `;
-    }).join('');
-    document.querySelector(".categories .row").innerHTML = result;
+        }).join('');
+        document.querySelector(".categories .row").innerHTML = result;
+    }
+    catch (error) {
+        document.querySelector(".categories .row").innerHTML = `<p>Loading...</p>`;
+    }
+    {
+        loader.classList.remove('active');
+    }
+
 };
 displayCategory();
+
+window.onscroll = function () {
+    const categories = document.querySelector('.categories');
+    const nav = document.querySelector('.header');
+    if (window.scrollY > categories.offsetTop) {
+        nav.classList.add('scroll-nav');
+    }
+    else {
+        nav.classList.remove('scroll-nav');
+    }
+
+}
+
 
